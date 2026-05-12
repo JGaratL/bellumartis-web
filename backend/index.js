c96734d701f603a4a8c1dbb06349b94d45df893c
JGaratL <joaquingaratluque@gmail.com>
2026-05-13 00:07:16 +0200

    Añadida ruta root para la API, q se borró en el anterior push

--- backend/index.js
+++ backend/index.js
@@ -593,6 +593,12 @@ SERVER
 */
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send(
+    "API Bellumartis funcionando correctamente"
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
